5e223e0959f511ac384f71123b2154bcf8e6c771
Andres Hernandez <ajhl1992@gmail.com>
2021-05-24 17:15:27 -0600

    Proyecto creado con create-React-APP y crea pagina Home

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from'react';
+import './App.css';
+import Homepage from'./homepage.component.jsx'
+
+
+function App() {
+  return <div><Homepage></Homepage></div>
+}
+
+export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const Homepage = () => (
+<div className= 'Homepage'>
+<div className= 'directory-menu'>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>Vestidos</h1>
+    <span className='subtitle'> Compra Ahora
+    </span>
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>Gorras</h1>
+    <span className='subtitle'>Compra Ahora</span>
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>Chaquetas</h1>
+    <span className='subtitle'>Compra Ahora</span>
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>Pantalones</h1>
+    <span className='subtitle'>Compra Ahora</span>
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+    <h1 className='title'>Camisas</h1>
+    <span className='subtitle'>Compra Ahora</span>
+</div>
+</div>
+</div>
+</div>
+)
+
+export default Homepage
